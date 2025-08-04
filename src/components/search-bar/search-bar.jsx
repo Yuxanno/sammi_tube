e3cdb4e673ae9157da51129d384febb0c958bc89
Yuxanno <ravshanovyuxanno1105@mail.ru>
2025-08-04 16:48:48 +0500

    finish commit

---
+++ src/components/search-bar/search-bar.jsx
@@ -0,0 +1,38 @@
+import { Paper } from "@mui/material";
+import { useState } from "react";
+import { colors } from "../../constants/colors";
+import { useNavigate } from "react-router-dom";
+
+const SearchBar = () => {
+  const [value, setValue] = useState("");
+  const navigate = useNavigate();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (value) {navigate(`/search/${value}`)}
+  };
+
+  return (
+    <Paper
+      component={"form"}
+      onSubmit={submitHandler}
+      sx={{
+        border: `1px solid ${colors.secondary}`,
+        pl: 2,
+        boxShadow: "none",
+        mr: 5,
+      }}
+    >
+      <input
+        type="text"
+        placeholder="Search..."
+        className="search-bar"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        required
+      />
+    </Paper>
+  );
+};
+
+export default SearchBar;
---
+++ src/service/api.service.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  params: {
+    maxResults: "50",
+  },
+  headers: {
+    "x-rapidapi-key": "3fb5e06285msh32a74e4e1924bcfp11d37cjsn93ba7d607530",
+    "x-rapidapi-host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const ApiService = {
+  async fetching(url) {
+    const response = await axios.get(`${BASE_URL}/${url}`, options);
+    return response.data;
+  },
+};
